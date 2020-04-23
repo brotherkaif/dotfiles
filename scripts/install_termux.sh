2a64b6ea449b24623cdb6cfcdadcb7d8a755a597
Kaif Ahmed <hello@kaif.me>
2020-04-23 17:14:22 +0100

    created a termux script!

---
+++ scripts/install_termux.sh
@@ -0,0 +1,107 @@
+#!/usr/bin/env bash
+echo "┌──────────────────────────────────┐"
+echo "│▜▘▙ ▌▞▀▖▀▛▘▞▀▖▌  ▌  ▞▀▖▀▛▘▜▘▞▀▖▙ ▌│"
+echo "│▐ ▌▌▌▚▄  ▌ ▙▄▌▌  ▌  ▙▄▌ ▌ ▐ ▌ ▌▌▌▌│"
+echo "│▐ ▌▝▌▖ ▌ ▌ ▌ ▌▌  ▌  ▌ ▌ ▌ ▐ ▌ ▌▌▝▌│"
+echo "│▀▘▘ ▘▝▀  ▘ ▘ ▘▀▀▘▀▀▘▘ ▘ ▘ ▀▘▝▀ ▘ ▘│"
+echo "└──────────────────────────────────┘"
+
+install_darwin () {
+	echo "This utility will install tools for Termux."
+	echo "WARNING: DO NOT run as root!"
+	echo "Proceed? (y/n)"
+	read resp
+	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+		# ---------------------------------------------
+		# Setup
+		# ---------------------------------------------
+		echo "┏━┓┏━╸╺┳╸╻ ╻┏━┓"
+		echo "┗━┓┣╸  ┃ ┃ ┃┣━┛"
+		echo "┗━┛┗━╸ ╹ ┗━┛╹  "
+
+		# Upgrade any already-installed packages
+		echo " ⡀⢀ ⣀⡀ ⢀⡀ ⡀⣀ ⢀⣀ ⢀⣸ ⢀⡀"
+		echo " ⠣⠼ ⡧⠜ ⣑⡺ ⠏  ⠣⠼ ⠣⠼ ⠣⠭"
+		pkg upgrade
+
+		# ---------------------------------------------
+		# Programming Languages and Frameworks
+		# ---------------------------------------------
+		echo "┏━╸┏━┓┏━┓┏┳┓┏━╸╻ ╻┏━┓┏━┓╻┏ ┏━┓"
+		echo "┣╸ ┣┳┛┣━┫┃┃┃┣╸ ┃╻┃┃ ┃┣┳┛┣┻┓┗━┓"
+		echo "╹  ╹┗╸╹ ╹╹ ╹┗━╸┗┻┛┗━┛╹┗╸╹ ╹┗━┛"
+
+		# node
+		echo " ⣀⡀ ⢀⡀ ⢀⣸ ⢀⡀"
+		echo " ⠇⠸ ⠣⠜ ⠣⠼ ⠣⠭"
+		echo "skipping: nvm installed as part of symlinking script"
+
+		# ---------------------------------------------
+		# Tools
+		# ---------------------------------------------
+		echo "╺┳╸┏━┓┏━┓╻  ┏━┓"
+		echo " ┃ ┃ ┃┃ ┃┃  ┗━┓"
+		echo " ╹ ┗━┛┗━┛┗━╸┗━┛"
+
+		# stow
+		echo "⢀⣀ ⣰⡀ ⢀⡀ ⡀ ⢀"
+		echo "⠭⠕ ⠘⠤ ⠣⠜ ⠱⠱⠃"
+		pkg install stow
+
+		# tree
+		echo " ⣰⡀ ⡀⣀ ⢀⡀ ⢀⡀"
+		echo " ⠘⠤ ⠏  ⠣⠭ ⠣⠭"
+		brew install tree
+
+		# htop
+		echo " ⣇⡀ ⣰⡀ ⢀⡀ ⣀⡀"
+		echo " ⠇⠸ ⠘⠤ ⠣⠜ ⡧⠜"
+		pkg install htop
+
+		# git
+		echo " ⢀⡀ ⠄ ⣰⡀"
+		echo " ⣑⡺ ⠇ ⠘⠤"
+		pkg install git
+
+		# tmux
+		echo " ⣰⡀ ⣀⣀  ⡀⢀ ⡀⢀"
+		echo " ⠘⠤ ⠇⠇⠇ ⠣⠼ ⠜⠣"
+		pkg install tmux
+
+		# nnn
+ 		echo"⣀⡀ ⣀⡀ ⣀⡀"
+ 		echo"⠇⠸ ⠇⠸ ⠇⠸"
+		pkg install nnn
+
+		# neovim
+		echo " ⣀⡀ ⢀⡀ ⢀⡀ ⡀⢀ ⠄ ⣀⣀ "
+		echo " ⠇⠸ ⠣⠭ ⠣⠜ ⠱⠃ ⠇ ⠇⠇⠇"
+		pkg install neovim
+
+		# irssi
+		echo " ⠄ ⡀⣀ ⢀⣀ ⢀⣀ ⠄"
+		echo " ⠇ ⠏  ⠭⠕ ⠭⠕ ⠇"
+		pkg install irssi
+
+		# ---------------------------------------------
+		# Stupid Terminal Nonsense
+		# ---------------------------------------------
+		echo "┏┓╻┏━┓┏┓╻┏━┓┏━╸┏┓╻┏━┓┏━╸"
+		echo "┃┗┫┃ ┃┃┗┫┗━┓┣╸ ┃┗┫┗━┓┣╸ "
+		echo "╹ ╹┗━┛╹ ╹┗━┛┗━╸╹ ╹┗━┛┗━╸"
+
+		# figlet
+		echo " ⣰⡁ ⠄ ⢀⡀ ⡇ ⢀⡀ ⣰⡀"
+		echo " ⢸  ⠇ ⣑⡺ ⠣ ⠣⠭ ⠘⠤"
+		pkg install figlet
+
+		# toilet
+		echo " ⣰⡀ ⢀⡀ ⠄ ⡇ ⢀⡀ ⣰⡀"
+		echo " ⠘⠤ ⠣⠜ ⠇ ⠣ ⠣⠭ ⠘⠤"
+		pkg install toilet
+	else
+		echo "INSTALLATION CANCELLED"
+	fi
+}
+
+install_darwin
