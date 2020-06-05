a35afd12328d778dca027d06cafe433a51d73ed2
Kaif Ahmed <hello@kaif.me>
2020-06-05 22:53:39 +0100

    tweaked debian install script

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -40,17 +40,6 @@ install_debian_packages () {
 		echo "⠣⠜ ⠇⠸   ⠇⠇⠇ ⣑⡺   ⠴⠥ ⠭⠕ ⠇⠸"
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-		# install snap
-		echo"⢀⣀ ⣀⡀ ⢀⣀ ⣀⡀"
-		echo"⠭⠕ ⠇⠸ ⠣⠼ ⡧⠜"
-		sudo apt install libsquashfuse0 squashfuse fuse -y
-		sudo apt install snapd -y
-
-		# install flatpak
-		echo " ⣰⡁ ⡇ ⢀⣀ ⣰⡀ ⣀⡀ ⢀⣀ ⡇⡠"
-		echo " ⢸  ⠣ ⠣⠼ ⠘⠤ ⡧⠜ ⠣⠼ ⠏⠢"
-		sudo apt install flatpak -y
-
 		# ---------------------------------------------
 		# Programming Languages and Frameworks
 		# ---------------------------------------------
@@ -137,7 +126,12 @@ install_debian_packages () {
 		# vscode
 		echo " ⡀⢀ ⢀⣀    ⢀⣀ ⢀⡀ ⢀⣸ ⢀⡀"
 		echo " ⠱⠃ ⠭⠕ ⠉⠉ ⠣⠤ ⠣⠜ ⠣⠼ ⠣⠭"
-		sudo snap install code --classic
+		curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+		sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+		sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+		sudo apt-get install apt-transport-https
+		sudo apt-get update
+		sudo apt-get install code
 		code --install-extension Shan.code-settings-sync
 
 		# ---------------------------------------------
