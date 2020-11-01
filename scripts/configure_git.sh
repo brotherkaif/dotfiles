ee75f768014a5be562a79b308a536a9401484aa0
Kaif Ahmed <dm@kaif.dev>
2020-11-01 16:47:25 +0000

    removed comments

--- scripts/configure_git.sh
+++ scripts/configure_git.sh
@@ -7,20 +7,19 @@ configure_git () {
 	echo "Proceed? (y/n)"
 	read resp
 	if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
-		# echo -e "\e[43;30m key generation \e[0m"
-		# echo "Generating key..."
-		# mkdir -p ~/.ssh && ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519 -C "dm@kaif.dev"
-		# echo "...done!"
-		# echo "KEY GENERATED:"
-		# cat ~/.ssh/id_ed25519.pub	
+		echo -e "\e[43;30m key generation \e[0m"
+		echo "Generating key..."
+		mkdir -p ~/.ssh && ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519 -C "dm@kaif.dev"
+		echo "...done!"
+		echo "KEY GENERATED:"
+		cat ~/.ssh/id_ed25519.pub	
 
-
-		# echo -e "\e[43;30m github testing \e[0m"
-		# echo "Test key with Github? (y/n)"
-		# read resp
-		# if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
-		# 	ssh -T git@github.com
-		# fi
+		echo -e "\e[43;30m github testing \e[0m"
+		echo "Test key with Github? (y/n)"
+		read resp
+		if [ "$resp" = 'y' -o "$resp" = 'Y' ] ; then
+			ssh -T git@github.com
+		fi
 
 		echo -e "\e[43;30m git config \e[0m"
 		echo "Setting user.email..."
