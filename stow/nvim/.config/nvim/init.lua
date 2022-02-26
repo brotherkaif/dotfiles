6bee895a95fba8dd16a338bcd167a7fedd2c6251
Kaif Ahmed <dm@kaif.dev>
2022-02-26 22:54:13 +0000

    feat(nvim): plugin improvements
    A significant update to the packer config and plugin structure. This new
    structure will hopefully make it easier up mangage plugin configs and
    manage plugin dependencies.

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -5,7 +5,7 @@
 -- |_|_| |_|_|\__(_)_|\__,_|\__,_|
 -- ===============================
 
-debug_mode=true
+debug_mode=false
 
 if debug_mode==true then
   print(' _| _ |_     _ ')
@@ -13,13 +13,8 @@ if debug_mode==true then
   print('             _|')
   print('LOADING CONFIGS...')
 end
+
 require('user.plugins')
--- require('user.plugins/treesitter-config')
--- require('user.configs/telescope')
--- require('user.plugins/nvim-cmp-config')
--- require('user.plugins/formatter-config')
--- require('user.plugins/which-key-config')
--- require('user.lsp')
 require('user.options')
 require('user.keymaps')
 
--- stow/nvim/.config/nvim/lua/user/configs/telescope.lua
+++ stow/nvim/.config/nvim/lua/user/configs/telescope.lua
@@ -2,28 +2,25 @@ local actions = require('telescope.actions')
 local custom_actions = {}
 local telescope = require('telescope')
 
-telescope.load_extension('fzf')
-telescope.load_extension('file_browser')
-
 telescope.setup({
-    defaults = {
+	defaults = {
 	hidden = true,
 	layout_strategy = 'flex',
 	sorting_strategy = 'ascending',
 	layout_config = {
-	    prompt_position = 'top',
+		prompt_position = 'top',
 	},
 	color_devicons = false,
-    },
-
-    extensions = {
-	file_browser = {
-	    dir_icon = '›',
-	    grouped = true,
 	},
-    },
+
+	extensions = {
+		file_browser = {
+			dir_icon = '›',
+			grouped = true,
+			hidden = true,
+		},
+	},
 })
 
-if debug_mode==true then
-  print('- user/configs/telescope.lua...OK!')
-end
+telescope.load_extension('fzf')
+telescope.load_extension('file_browser')
