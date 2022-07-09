b6408d3968f5f24da1eb64dc72494c79755787da
kaifahmed <kaifahmed@penguin>
2022-07-09 23:24:31 +0100

    feat(nvim): minor edits

--- nvim/.config/nvim/plugin/nvim-treesitter.lua
+++ nvim/.config/nvim/plugin/nvim-treesitter.lua
@@ -116,20 +116,12 @@ languages = {
 
 tsconfig.setup({
 	ensure_installed = languages,
+	ignore_install = { "" },
 	sync_install = false,
-	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "" }, -- list of language that will be disabled
+		enable = true,
+		disable = { "" },
 		additional_vim_regex_highlighting = true,
 	},
 	indent = { enable = true, disable = { "yaml" } },
-	rainbow = {
-		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
-	}
 })
