604fcbd07f6c5ca2ff6562c25ea2252ac10e8f85
Kaif Ahmed <dm@kaif.dev>
2022-09-14 18:50:35 +0100

    feat(nvim): debugging jest tests
    Finally cracked debugging jest tests. It's not perfect, there are still
    errors that pop up as a result of sourcemaps not being defined. But the
    debug still runs as intended.

--- nvim/lua/config-dap.lua
+++ nvim/lua/config-dap.lua
@@ -1,25 +1,29 @@
-local dap, dapui = require("dap"), require("dapui")
+local dap, dapui = require('dap'), require('dapui')
 
-local install_root_dir = vim.fn.stdpath "data" .. "/mason/packages"
+-- packages are installed via mason, so we need to construct the path 
+local install_root_dir = vim.fn.stdpath('data') .. '/mason/packages'
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', "<F5>", "<Cmd>lua require'dap'.continue()<CR>", opts)
-vim.keymap.set('n', "<S-F5>", "<Cmd>lua require'dap'.stop()<CR>", opts)
-vim.keymap.set('n', "<F9>", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
-vim.keymap.set('n', "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
-vim.keymap.set('n', "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
-vim.keymap.set('n', "<S-F11>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
+vim.keymap.set('n', '<F5>', '<Cmd>lua require("dap").continue()<CR>', opts)
+vim.keymap.set('n', '<S-F5>', '<Cmd>lua require("dap").stop()<CR>', opts)
+vim.keymap.set('n', '<F9>', '<Cmd>lua require("dap").toggle_breakpoint()<CR>', opts)
+vim.keymap.set('n', '<F10>', '<Cmd>lua require("dap").step_over()<CR>', opts)
+vim.keymap.set('n', '<F11>', '<Cmd>lua require("dap").step_into()<CR>', opts)
+vim.keymap.set('n', '<S-F11>', '<Cmd>lua require("dap").step_out()<CR>', opts)
 
-dap.adapters.node2 = {
+dap.adapters.node = {
 	type = 'executable',
 	command = 'node',
 	args = { install_root_dir .. '/node-debug2-adapter/out/src/nodeDebug.js' },
 }
 
-dap.configurations.javascript = {
+-- load any launch.json configs from a project .vscode directory
+require('dap.ext.vscode').load_launchjs(nil, { node = { 'javascript', 'typescript' } })
+
+local node_configurations = {
 	{
 		name = 'Launch',
-		type = 'node2',
+		type = 'node',
 		request = 'launch',
 		program = '${file}',
 		cwd = vim.fn.getcwd(),
@@ -30,40 +34,60 @@ dap.configurations.javascript = {
 	{
 		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
 		name = 'Attach to process',
-		type = 'node2',
+		type = 'node',
 		request = 'attach',
 		processId = require 'dap.utils'.pick_process,
 	},
+	{
+		name = 'BBC-CC: All Jest tests',
+		type = 'node',
+		request = 'launch',
+		program = '${workspaceFolder}/node_modules/jest/bin/jest.js',
+		env = {
+			LOG_LEVEL = 'silent'
+		},
+		args = {
+			'-c',
+			'${workspaceFolder}/jest.config.js',
+			'--verbose',
+			'-i',
+			'--no-cache'
+		},
+		console = 'integratedTerminal',
+		internalConsoleOptions = 'neverOpen'
+	},
+	{
+		name = 'BBC-CC: Single Jest test',
+		type = 'node',
+		request = 'launch',
+		program = '${workspaceFolder}/node_modules/jest/bin/jest.js',
+		env = {
+			LOG_LEVEL = 'silent'
+		},
+		args = {
+			'${file}',
+			'-c',
+			'${workspaceFolder}/jest.config.js',
+			'--verbose',
+			'-i',
+			'--no-cache'
+		},
+		console = 'integratedTerminal',
+		internalConsoleOptions = 'neverOpen'
+	}
 }
 
-dap.configurations.typescript = {
-	{
-		name = 'Launch',
-		type = 'node2',
-		request = 'launch',
-		program = '${file}',
-		cwd = vim.fn.getcwd(),
-		sourceMaps = true,
-		protocol = 'inspector',
-		console = 'integratedTerminal',
-	},
-	{
-		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
-		name = 'Attach to process',
-		type = 'node2',
-		request = 'attach',
-		processId = require 'dap.utils'.pick_process,
-	},
-}
+dap.configurations.javascript = node_configurations
+dap.configurations.typescript = node_configurations
 
 dapui.setup()
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dap.listeners.after.event_initialized['dapui_config'] = function()
 	dapui.open({})
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.event_terminated['dapui_config'] = function()
 	dapui.close({})
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited['dapui_config'] = function()
 	dapui.close({})
 end
