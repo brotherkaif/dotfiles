a76a437a49278cf1d989e7bd3dfb25ebf140df01
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2022-11-30 13:59:42 +0000

    feat(lvim): added hop bindings

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,11 +1,41 @@
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.lsp.diagnostics.virtual_text = true
 lvim.colorscheme = "tokyonight"
+
+-- KEYMAPS
+lvim.builtin.which_key.mappings["p"] = {
+  ":Telescope<CR>", "Command Palette"
+}
+
+lvim.builtin.which_key.mappings[" "] = {
+  name = "easymotion",
+  s = {":HopChar<cr>", "Search character"},
+  f = {":HopChar1AC<cr>", "Find character forwards"},
+  F = {":HopChar1BC<cr>", "Find character backwards"},
+  t = {":HopChar1AC<cr>", "Til character forwards"},
+  T = {":HopChar1BC<cr>", "Til character backwards"},
+  w = {":HopWordAC<cr>", "Start of word forwards"},
+  b = {":HopWordBC<cr>", "Start of word backwards"},
+  l = {":HopWordAC<cr>", "Matches beginning & ending of word forwards"},
+  h = {":HopWordBC<cr>", "Matches beginning & ending of word backwards"},
+  e = {":HopWordAC<cr>", "End of word forwards"},
+  ge = {":HopWordBC<cr>", "End of word backwards"},
+  j = {":HopLineStartAC<cr>", "Start of line forwards"},
+  k = {":HopLineStartBC<cr>", "Start of line backwards"},
+  ["/"] = {":HopPattern<cr>", "Search n-character"},
+}
 
 -- PLUGINS
 lvim.plugins = {
   { "lunarvim/colorschemes" },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim" }
+  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
 }
 
 -- PACKAGES
