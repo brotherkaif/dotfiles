b39f3ea552692bf6b7718836c1ec52064650c978
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2022-12-14 18:27:00 +0000

    feat(lvim): refactored config into separate files

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,71 +1,7 @@
-lvim.format_on_save = false
+reload("user.ide")
+reload("user.keymaps")
+reload("user.plugins")
+
+lvim.format_on_save = true
 lvim.lsp.diagnostics.virtual_text = true
-lvim.colorscheme = "tokyonight"
-
--- KEYMAPS
-lvim.builtin.which_key.mappings["p"] = {
-  ":Telescope<CR>", "Command Palette"
-}
-
-lvim.builtin.which_key.mappings[" "] = {
-  name = "easymotion",
-  s = {":HopChar<cr>", "Search character"},
-  f = {":HopChar1AC<cr>", "Find character forwards"},
-  F = {":HopChar1BC<cr>", "Find character backwards"},
-  t = {":HopChar1AC<cr>", "Til character forwards"},
-  T = {":HopChar1BC<cr>", "Til character backwards"},
-  w = {":HopWordAC<cr>", "Start of word forwards"},
-  b = {":HopWordBC<cr>", "Start of word backwards"},
-  l = {":HopWordAC<cr>", "Matches beginning & ending of word forwards"},
-  h = {":HopWordBC<cr>", "Matches beginning & ending of word backwards"},
-  e = {":HopWordAC<cr>", "End of word forwards"},
-  ge = {":HopWordBC<cr>", "End of word backwards"},
-  j = {":HopLineStartAC<cr>", "Start of line forwards"},
-  k = {":HopLineStartBC<cr>", "Start of line backwards"},
-  ["/"] = {":HopPattern<cr>", "Search n-character"},
-}
-
--- PLUGINS
-lvim.plugins = {
-  { "lunarvim/colorschemes" },
-  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-  {
-    "phaazon/hop.nvim",
-    event = "BufRead",
-    config = function()
-      require("hop").setup()
-    end,
-  },
-}
-
--- PACKAGES
-require('mason-tool-installer').setup {
-  ensure_installed = {
-    -- javascript, typescript
-    'prettierd',
-    'eslint_d',
-  },
-  auto_update = true,
-  run_on_start = true,
-  start_delay = 3000, -- 3 second delay
-}
-
--- FORMATTERS
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "prettierd" },
-}
-
--- LINTERS
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "eslint_d" },
-}
-
--- CODE ACTIONS
--- local code_actions = require "lvim.lsp.null-ls.code_actions"
--- code_actions.setup {
---   {
---     command = "proselint"
---   },
--- }
+lvim.colorscheme = "darkplus"
---
+++ lvim/lua/user/plugins.lua
@@ -0,0 +1,28 @@
+lvim.plugins = {
+  { "lunarvim/colorschemes" },
+  { "WhoIsSethDaniel/mason-tool-installer.nvim" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
+  { "zbirenbaum/copilot.lua",
+    event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup {
+          plugin_manager_path = get_runtime_dir() .. "/site/pack/packer",
+        }
+      end, 100)
+    end,
+  },
+  { "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua", "nvim-cmp" },
+  },
+}
+
+-- Can not be placed into the config method of the plugins.
+lvim.builtin.cmp.formatting.source_names["copilot"] = "(Copilot)"
+table.insert(lvim.builtin.cmp.sources, 1, { name = "copilot" })
