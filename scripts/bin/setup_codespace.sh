0968dcb030162010689062a6fa976b0be5778f29
Kaif Ahmed <dm@kaif.dev>
2023-01-13 23:55:56 +0000

    feat(scripts): improved setup

--- scripts/bin/setup_codespace.sh
+++ scripts/bin/setup_codespace.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+isDotfilesRepo() {
+    if [[ -f /etc/os-release ]] && [[ -d /workspaces/dotfiles ]]; then
+        return true
+    fi
+
+    return false
+}
+
 touch ~/RAN-CODESPACE-SCRIPT
 cd $HOME
 
@@ -6,6 +14,8 @@ cd $HOME
 export SUDO_ASKPASS=/bin/true
 
 # Install neovim
+rm $HOME/.local/bin/nvim
+rm $HOME/.config/nvim
 wget -O /tmp/nvim.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 sudo apt install /tmp/nvim.deb -y
 rm /tmp/nvim.deb
