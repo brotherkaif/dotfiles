42e1e8b396d804a8bc5d872f658ccb6d905222f9
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-02-08 22:15:35 +0000

    feat(nvim): add which-key plugin

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,30 @@
+return {
+  -- which-key - popup menu for keybindings
+  'folke/which-key.nvim',
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    local wk = require('which-key')
+    wk.setup()
+    wk.register({
+      [" "] = {
+        name = "Hop/Easymotion",
+      },
+      t = {
+        name = "[T]erminal",
+      },
+      s = {
+        name = "[S]earch",
+      },
+      c = {
+        name = "[C]ode",
+      },
+      r = {
+        name = "[R]e",
+      },
+      w = {
+        name = "[W]orkspace",
+      },
+    }, { prefix = "<leader>" })
+  end,
+}
