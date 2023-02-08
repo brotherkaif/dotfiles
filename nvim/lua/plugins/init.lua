7bb528663572e0d12cf6bd2dd1e80f52dcd7a5c0
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-02-08 19:18:20 +0000

    refactor(nvim): extract tpope config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,8 +1,3 @@
 return {
-  -- The obligatory Tim Pope section
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-  'tpope/vim-sleuth',
-
   'hinell/move.nvim', -- Line manipulation
 }
---
+++ nvim/lua/plugins/tpope.lua
@@ -0,0 +1,6 @@
+return {
+  -- The obligatory Tim Pope section
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+  'tpope/vim-sleuth',
+}
