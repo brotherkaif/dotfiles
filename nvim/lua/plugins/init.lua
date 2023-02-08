0109b95d1c04a9ae311fdaec2ce799751a82dd4c
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-02-08 19:10:48 +0000

    refactor(nvim): extract indent-blankline config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,16 +8,6 @@ return {
   'tpope/vim-sleuth',
 
   {
-    -- indent-blankline.nvim - Add indentation guides even on blank lines
-    -- See `:help indent_blankline.txt`
-    'lukas-reineke/indent-blankline.nvim',
-    config = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    }
-  },
-
-  {
     -- Comment.nvim - Comment toggling
     'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
     config = true
