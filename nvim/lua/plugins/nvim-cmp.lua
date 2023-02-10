1866d7d44a00fcbf15a3f858b4e7d9c13b7b9fe3
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-02-10 18:31:11 +0000

    feat(nvim): add snippets
    Added a snippets library to nvim-cmp.

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,27 +1,37 @@
 return {
   -- nvim-cmp - Autocompletion
   'hrsh7th/nvim-cmp',
-  dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets',
+  },
   config = function ()
-    -- nvim-cmp setup
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
 
+    -- nvim-cmp configuration
     cmp.setup {
+      -- integrate with luasnip
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
         end,
       },
+
+      -- define keymaps
       mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
+
         ['<C-y>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
         },
-                    ['<C-e>'] = cmp.mapping.close(),
+
+        ['<C-e>'] = cmp.mapping.close(),
 
         ['<C-n>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
@@ -55,10 +65,15 @@ return {
           end
         end, { 'i', 's' }),
       },
+
+      -- define sources
       sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
       },
     }
+
+    -- integrate friendly-snippets with luasnip
+    require("luasnip.loaders.from_vscode").lazy_load()
   end
 }
