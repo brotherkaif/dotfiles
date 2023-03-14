0fd7f7a1e874a3e09df2ddc04c16fa7666256186
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-03-14 09:25:51 +0000

    chore(nvim): remove old configs

--- nvim/OLD/lua/config-mac.lua
+++
@@ -1,5 +0,0 @@
-if vim.fn.has('mac') == 1 then
-	vim.cmd('colorscheme rose-pine')
-else
-	vim.cmd('colorscheme pablo')
-end
--- nvim/OLD/plugin/vim-floaterm.vim
+++
@@ -1,2 +0,0 @@
-let g:floaterm_height = 0.80
-let g:floaterm_width = 0.80
