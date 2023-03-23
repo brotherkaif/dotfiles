7f9344f0889f0baf3e2afe3d29c3b291d907908c
Kaif Ahmed <dm@kaif.dev>
2023-03-23 19:01:38 +0000

    fix(scripts): update gh installation

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -93,9 +93,12 @@ case $(detectOS) in
 	type -p curl >/dev/null || sudo apt install curl -y
 	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 	&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
-	&& sudo apt install gh -y
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	sudo apt update
+	sudo apt install gh -y
+
+	echo "Installing dev container CLI"
+	npm install -g @devcontainers/cli
 
 	echo "Installing Flatpak"
 	# installation instructions: https://flatpak.org/setup/Debian
