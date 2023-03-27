8dfc80f65b5717387e70af33352b8ecfb9783078
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-03-27 17:31:29 +0100

    feat(nvim): update theme to dim
    The theme has now been changed to "Dim" which offers an ANSI compatible
    colourscheme. This has the advantage of working on a wider variety of
    terminal emulators (as a lot of them do not offer truecolor support).

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,22 +1,10 @@
 return {
   {
-    -- rose-pine - the most comfortable theme
-    'rose-pine/neovim',
-    name = 'rose-pine',
+    -- dim: Default IMproved
+    'jeffkreeftmeijer/vim-dim',
     lazy = false,
-    priority = 1000,
-    -- config = function()
-    --     require('rose-pine').setup()
-    --     vim.cmd('colorscheme rose-pine')
-    -- end
-  },
-  {
-    -- github-nvim-theme - the most professional theme
-    'projekt0n/github-nvim-theme',
-    lazy = false,
-    branch = '0.0.x',
     config = function()
-      vim.cmd('colorscheme github_dark_default')
+      vim.cmd('colorscheme dim')
     end
   },
 }
