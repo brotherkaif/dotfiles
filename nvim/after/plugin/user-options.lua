9985fe8e1576e8c77a7d93936fe7cbef6cb14cd4
Kaif Ahmed <dm@kaif.dev>
2023-04-16 12:10:37 +0100

    feat(nvim): add editorconfig support
    Added an editorconfig file and tweaked user options to accommodate for
    it.

--- nvim/after/plugin/user-options.lua
+++ nvim/after/plugin/user-options.lua
@@ -1,90 +1,69 @@
 local options = {
 	-- set highlight on search
 	hlsearch = false,
-
 	-- enable mouse mode
 	mouse = 'a',
-
 	-- enable break indent
 	breakindent = true,
-
 	-- case insensitive searching UNLESS /C or capital in search
 	ignorecase = true,
 	smartcase = true,
-
 	-- faster completion (4000ms default)
 	updatetime = 250,
-
 	-- set completeopt to have a better completion experience
 	completeopt = 'menuone,noselect',
-
 	-- creates a backup file
 	backup = false,
-
 	-- so that `` is visible in markdown files
 	conceallevel = 0,
-
 	-- highlight the current line
 	cursorline = true,
-
 	-- disable terminal error bell
 	errorbells = false,
-
 	-- the encoding written to a file
 	fileencoding = 'utf-8',
-
 	-- the font used in graphical neovim applications
 	guifont = 'Noto Sans Mono:h12',
-
 	-- pop up menu height
 	pumheight = 10,
-
 	-- set relative numbered lines
 	-- relativenumber = true,
 
 	-- offset vertical scrolling
 	scrolloff = 8,
-
 	-- highlight matches on search pattern in real time
 	showmatch = true,
-
 	-- we don't need to see things like -- INSERT -- anymore
-	-- showmode = false,
-
+	showmode = false,
 	-- always show tabs
 	-- showtabline = 2,
 
 	-- offset horizontal scrolling
 	sidescrolloff = 8,
-
 	-- make indenting smarter again
 	-- smartindent = true,
 
 	-- force all horizontal splits to go below current window
 	splitbelow = true,
-
 	-- force all vertical splits to go to the right of current window
 	splitright = true,
-
 	-- creates a swapfile
 	swapfile = false,
-
+	-- set how many chars a tab is represented by
+	tabstop = 4,
+	-- set shifting chars amount
+	shiftwidth = 2,
 	-- convert tabs to spaces
 	-- expandtab = false,
 
 	-- set term gui colors (most terminals support this)
 	termguicolors = false,
-
 	-- time to wait for a mapped sequence to complete (in milliseconds)
 	timeoutlen = 1000,
-
 	-- enable persistent undo
 	undofile = true,
-
-
 	-- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 	writebackup = false,
-
 	-- command height
 	-- ch = 0,
 }
@@ -92,16 +71,12 @@ local options = {
 local window_options = {
 	-- sets the fold level
 	foldlevel = 2,
-
 	-- the kind of folding used for the current window
 	foldmethod = 'indent',
-
 	-- display unprintable characters like tab and trailing spaces
-	list = false,
-
+	-- list = true,
 	-- enable the signs column
 	signcolumn = 'yes',
-
 	-- Make line numbers default
 	number = true,
 }
