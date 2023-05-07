b60249091553440e00c6b8916d197a7c66af991f
Kaif Ahmed <dm@kaif.dev>
2023-05-07 02:32:04 +0100

    feat(nvim): swap plugin

--- nvim/lua/plugins/indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- indent-blankline.nvim - Add indentation guides even on blank lines
-  -- See `:help indent_blankline.txt`
-  'lukas-reineke/indent-blankline.nvim',
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
-  }
-}
