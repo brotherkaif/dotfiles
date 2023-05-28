ec9228707b9b1ab6c7443d0392fcca23745e5dda
Kaif Ahmed <hello@kaif.me>
2023-05-28 22:44:36 +0000

    fix(nvim): disable help installation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,8 @@ return {
     -- See `:help nvim-treesitter`
     require('nvim-treesitter.configs').setup {
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript' },
+			ignore_install = { "help" },
 
       highlight = { enable = true },
       indent = { enable = true },
