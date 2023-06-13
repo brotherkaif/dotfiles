af288f2f4cefa37a89dd02dcfb2557cbbaf8caba
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-06-13 09:44:25 +0100

    feat(nvim): add mac override

--- nvim/after/plugin/user-options.lua
+++ nvim/after/plugin/user-options.lua
@@ -4,4 +4,10 @@ vim.opt["sidescrolloff"] = 8
 vim.wo["foldlevel"] = 4
 vim.wo["foldmethod"] = "indent"
 
-vim.cmd("colorscheme randomhue")
+-- Mac only options
+if vim.fn.has("mac") then
+  vim.opt["termguicolors"] = false
+else
+  vim.opt["termguicolors"] = true
+	vim.cmd("colorscheme randomhue")
+end
