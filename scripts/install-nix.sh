cd4c3e2086dcf884edeaab1f1059282ee7713cfd
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-07-05 17:32:08 +0100

    feat(nixpkgs): add nix support
    Experimenting with using nix as my main package manager across the
    board. Adding config for darwin.

---
+++ scripts/install-nix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Import detectOS
+. ./detect-os.sh
+
+echo "INSTALL NIX"
+# https://nixos.org/download.html
+
+case $(detectOS) in
+    "darwin")
+			echo "Darwin detected. Installing..."
+
+			sh <(curl -L https://nixos.org/nix/install)
+
+			# nix-darwin required on macos
+			# http://daiderd.com/nix-darwin/
+			nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
+			./result/bin/darwin-installer
+			;;
+	*)
+			echo "Could not detect OS! Skipping."
+			echo "Value recieved: $(detectOS)"
+			;;
+esac
