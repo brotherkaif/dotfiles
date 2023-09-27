60cdc1c2218f0b705781e661b4790019129f3374
Kaif Ashek Ahmed <brotherkaif@pop-os.localdomain>
2023-09-27 20:25:27 +0100

    feat(nvim): disable conceallevel

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,20 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.wo["foldlevel"] = 4
-vim.wo["foldmethod"] = "indent"
 
+-- Fold level
+vim.wo.foldlevel = 4
+vim.wo.foldmethod = "indent"
+
+-- Disable mouse pop up
 vim.cmd.aunmenu({ "PopUp.How-to\\ disable\\ mouse" })
 vim.cmd.aunmenu({ "PopUp.-1-" })
 
+-- Disable conceal
+vim.o.conceallevel = 0
+
+-- Terminal truecolor fallback
 if os.getenv("COLORTERM") ~= "truecolor" then
   -- disable termguicolors
-  vim.opt["termguicolors"] = false
+  vim.opt.termguicolors = false
 
   -- set default colorscheme
   vim.cmd("colorscheme default")
 else
   -- enable termguicolors
-  vim.opt["termguicolors"] = true
+  vim.opt.termguicolors = true
   vim.cmd("colorscheme default")
 end
