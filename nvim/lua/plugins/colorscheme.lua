e37d2ca526708f8cdcc51572865f92f338066915
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-09-29 18:00:51 +0100

    feat(nvim): simplify theming

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,57 +1,17 @@
-local isTrueColor = os.getenv("COLORTERM") == "truecolor"
-
 return {
-  -- add mini.hues
-  {
-    "echasnovski/mini.hues",
-    keys = function()
-      -- check if truecolor is enabled in terminal
-      if isTrueColor then
-        -- if so, set Random Hue keymap
-        return {
-          {
-            "<leader>uu",
-            function()
-              vim.cmd("colorscheme randomhue")
-            end,
-            desc = "Random Hue",
-          },
-        }
-      else
-        -- otherwise, do not set a keymap
-        return {}
-      end
-    end,
-  },
-
-  -- add auto-dark-mode.nvim
+  -- auto-dark-mode.nvim
   {
     "f-person/auto-dark-mode.nvim",
     config = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option("background", "dark")
-        if isTrueColor then
-          vim.cmd("colorscheme randomhue")
-        else
-          -- if not, disable termguicolors
-          vim.opt["termguicolors"] = false
-
-          -- and fall back to non truecolor colorscheme
-          vim.cmd("colorscheme lunaperche")
-        end
+        -- vim.cmd("colorscheme lunaperche")
       end,
+
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
-        if isTrueColor then
-          vim.cmd("colorscheme randomhue")
-        else
-          -- if not, disable termguicolors
-          vim.opt["termguicolors"] = false
-
-          -- and fall back to non truecolor colorscheme
-          vim.cmd("colorscheme lunaperche")
-        end
+        -- vim.cmd("colorscheme lunaperche")
       end,
     },
   },
@@ -61,20 +21,16 @@ return {
     "LazyVim/LazyVim",
     opts = function()
       -- check if truecolor is enabled in terminal
-      if isTrueColor then
-        -- otherwise, set colorscheme to randomhue
-        return {
-          colorscheme = "randomhue",
-        }
-      else
-        -- if not, disable termguicolors
-        vim.opt["termguicolors"] = false
+      local trueColor = os.getenv("COLORTERM") == "truecolor"
 
-        -- and fall back to non truecolor colorscheme
-        return {
-          colorscheme = "lunaperche",
-        }
+      if not trueColor then
+        vim.opt.termguicolors = false
+        vim.o.pumblend = 0
       end
+
+      return {
+        colorscheme = "lunaperche",
+      }
     end,
   },
 }
