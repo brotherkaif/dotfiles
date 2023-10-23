76986a615259239dcfcb839cf4d430bd518958fd
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-10-23 13:38:22 +0100

    chore: restore options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Folding
+vim.wo.foldmethod = "indent"
+
+-- Disable mouse pop up
+vim.cmd.aunmenu({ "PopUp.How-to\\ disable\\ mouse" })
+vim.cmd.aunmenu({ "PopUp.-1-" })
+
+-- Disable conceal
+vim.opt.conceallevel = 0
+
+-- Truecolor fallback
+local trueColor = os.getenv("COLORTERM") == "truecolor"
+if not trueColor then
+  vim.opt.termguicolors = false
+  vim.opt.pumblend = 0
+end
