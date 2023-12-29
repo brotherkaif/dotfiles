8ad4f8d5732abc4fc7f52e581d8a30dd156ee9f9
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2023-12-29 15:14:10 +0000

    feat(nvim): add lsp keymaps

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -33,20 +33,26 @@ return {
 				vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
 			end
 
-			nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-			nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+			nmap('<space>e', vim.diagnostic.open_float, 'Open Float')
+			nmap('<space>q', vim.diagnostic.setloclist, 'Set Local List')
+			nmap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
+			nmap('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
 
 			-- See `:help K` for why this keymap
 			nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
 			-- Lesser used LSP functionality
-			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-			nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-			nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+			nmap('gd', vim.lsp.buf.definition, '[g]oto [d]efinition')
+			nmap('gD', vim.lsp.buf.declaration, '[g]oto [D]eclaration')
+			nmap('gi', vim.lsp.buf.implementation, '[g]oto [i]mplementation')
+			nmap('gr', vim.lsp.buf.references, '[g]oto [r]eferences')
+			nmap('<space>D', vim.lsp.buf.type_definition, 'type [D]efinition')
+			nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[w]orkspace [a]dd Folder')
+			nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[w]orkspace [r]emove Folder')
 			nmap('<leader>wl', function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, '[W]orkspace [L]ist Folders')
+			end, '[w]orkspace [l]ist folders')
 
 			-- Create a command `:Format` local to the LSP buffer
 			vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
