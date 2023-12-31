2a117bcd9d09f1fb0125fe1964f5d3da51ae5b71
Kaif Ashek Ahmed <dm@kaif.dev>
2023-12-31 00:33:53 +0000

    chore(nvim): format file

--- nvim/lua/custom/plugins/mini-hipatterns.lua
+++ nvim/lua/custom/plugins/mini-hipatterns.lua
@@ -1,11 +1,11 @@
 return {
 	'echasnovski/mini.hipatterns',
 	opts = {
-    highlighters = {
-      fixme = { pattern = 'FIXME:', group = 'MiniHipatternsFixme' },
-      hack  = { pattern = 'HACK:',  group = 'MiniHipatternsHack'  },
-      todo  = { pattern = 'TODO:',  group = 'MiniHipatternsTodo'  },
-      note  = { pattern = 'NOTE:',  group = 'MiniHipatternsNote'  },
-    }
-  }
+		highlighters = {
+			fixme = { pattern = 'FIXME:', group = 'MiniHipatternsFixme' },
+			hack  = { pattern = 'HACK:', group = 'MiniHipatternsHack' },
+			todo  = { pattern = 'TODO:', group = 'MiniHipatternsTodo' },
+			note  = { pattern = 'NOTE:', group = 'MiniHipatternsNote' },
+		}
+	}
 }
