9e6f5a17c9672a85e35aa57f739db1ae0e452ee3
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-01-10 18:55:26 +0000

    fix: resolve mouse scroll issue with mini animate

--- nvim/lua/custom/plugins/mini-nvim.lua
+++ nvim/lua/custom/plugins/mini-nvim.lua
@@ -5,6 +5,37 @@ return {
 		'nvim-tree/nvim-web-devicons',
 	},
 	config = function()
+		local animate_config = function()
+			-- don't use animate when scrolling with the mouse
+			local mouse_scrolled = false
+			for _, scroll in ipairs({ "Up", "Down" }) do
+				local key = "<ScrollWheel" .. scroll .. ">"
+				vim.keymap.set({ "", "i" }, key, function()
+					mouse_scrolled = true
+					return key
+				end, { expr = true })
+			end
+
+			local animate = require("mini.animate")
+			return {
+				resize = {
+					timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+				},
+				scroll = {
+					timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+					subscroll = animate.gen_subscroll.equal({
+						predicate = function(total_scroll)
+							if mouse_scrolled then
+								mouse_scrolled = false
+								return false
+							end
+							return total_scroll > 1
+						end,
+					}),
+				},
+			}
+		end
+
 		local basics_config = {
 			options = {
 				basic = true,
@@ -137,7 +168,7 @@ return {
 
 		require("mini.ai").setup()
 		-- require("mini.align").setup()
-		require("mini.animate").setup()
+		require("mini.animate").setup(animate_config())
 		-- require("mini.base16").setup()
 		require("mini.basics").setup(basics_config)
 		require("mini.bracketed").setup()
