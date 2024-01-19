ddd7a4bda24213723eb210b1cb3aaf1a127bcb2a
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-01-19 11:40:53 +0000

    feat(nvim): add c language support

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -74,7 +74,7 @@ return {
 		--  If you want to override the default filetypes that your language server will attach to you can
 		--  define the property 'filetypes' to the map in question.
 		local servers = {
-			-- clangd = {},
+			clangd = {},
 			-- gopls = {},
 			-- pyright = {},
 			-- rust_analyzer = {},
@@ -87,7 +87,7 @@ return {
 					workspace = { checkThirdParty = false },
 					telemetry = { enable = false },
 					-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-					-- diagnostics = { disable = { 'missing-fields' } },
+					diagnostics = { disable = { 'missing-fields' } },
 				},
 			},
 		}
