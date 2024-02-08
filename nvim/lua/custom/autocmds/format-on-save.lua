62b34f30b4854efe3d792938739f49415c9549e9
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-02-08 17:13:52 +0000

    fix(nvim): disable autoformat behaviour

--- nvim/lua/custom/autocmds/format-on-save.lua
+++ nvim/lua/custom/autocmds/format-on-save.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_create_autocmd('BufWritePre', {
-  callback = function()
-    vim.lsp.buf.format({ async = true })
-  end,
-})
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   callback = function()
+--     vim.lsp.buf.format({ async = true })
+--   end,
+-- })
