e8c23ae72881c50f8475eec6ba2a9eac4bc3001a
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-06-21 15:10:56 +0100

    feat(nvim): add typescript config for conform

--- nvim/lua/src/plugins/conform.lua
+++ nvim/lua/src/plugins/conform.lua
@@ -2,6 +2,7 @@ require('conform').setup({
   -- Map of filetype to formatters
   formatters_by_ft = {
     javascript = { 'eslint_d', 'prettier' },
+    typescript = { 'eslint_d', 'prettier' },
     json = { 'prettier' },
     lua = { 'stylua' },
   },
