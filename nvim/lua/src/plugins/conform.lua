ef193d032a28426f9852cec5389dcfcaed1f59e9
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-09-06 18:31:24 +0100

    fix: remove eslint_d

--- nvim/lua/src/plugins/conform.lua
+++ nvim/lua/src/plugins/conform.lua
@@ -1,8 +1,8 @@
 require('conform').setup({
   -- Map of filetype to formatters
   formatters_by_ft = {
-    javascript = { 'eslint_d', 'prettier' },
-    typescript = { 'eslint_d' },
+    javascript = { 'prettier' },
+    typescript = { 'prettier' },
     json = { 'prettier' },
     lua = { 'stylua' },
   },
