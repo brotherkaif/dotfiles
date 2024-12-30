f3ff3c82b12b61f4b5a8e0c1df2610e27146edbf
Kaif Ahmed <kaif.ahmed@bbc.co.uk>
2024-12-30 20:36:05 +0200

    feat: update conform config

--- private_dot_config/nvim/lua/src/plugins/conform.lua
+++ private_dot_config/nvim/lua/src/plugins/conform.lua
@@ -1,11 +1,56 @@
 MiniDeps.add("stevearc/conform.nvim")
 
-require("conform").setup({
-	-- Map of filetype to formatters
-	formatters_by_ft = {
-		javascript = { "prettier", "eslint" },
-		typescript = { "prettier", "eslint" },
-		json = { "prettier" },
-		lua = { "stylua" },
-	},
-})
+local supported_types = {
+	"astro",
+	"css",
+	"graphql",
+	"handlebars",
+	"html",
+	"javascript",
+	"javascriptreact",
+	"json",
+	"jsonc",
+	"less",
+	"markdown",
+	"markdown.mdx",
+	"scss",
+	"svelte",
+	"typescript",
+	"typescriptreact",
+	"vue",
+	"yaml",
+}
+
+local opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.lua = { "stylua" }
+
+      for _, ft in ipairs(supported_types) do
+        opts.formatters_by_ft[ft] = { "biome", "prettier", stop_after_first = true }
+      end
+
+      opts.formatters = opts.formatters or {}
+
+      opts.formatters.biome = {
+        require_cwd = true,
+      }
+      opts.formatters.prettier = {
+        require_cwd = true,
+      }
+
+      opts.format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        -- Disable autoformat for files in a certain path
+        local bufname = vim.api.nvim_buf_get_name(bufnr)
+        if bufname:match("/node_modules/") then
+          return
+        end
+        -- ...additional logic...
+        return { timeout_ms = 500, lsp_format = "never" }
+      end
+    end
+
+require("conform").setup(opts)
--- private_dot_config/nvim/lua/src/plugins/lsp.lua
+++ private_dot_config/nvim/lua/src/plugins/lsp.lua
@@ -11,6 +11,7 @@ require("mason").setup()
 local servers = {
 	clangd = {},
 	cssls = {},
+	eslint = {},
 	gopls = {},
 	html = {},
 	jsonls = {},
