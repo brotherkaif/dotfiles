1a997c69a8b468fd2a88b6e942f8dda2345cc81e
Kaif Ashek Ahmed <github.banister767@passinbox.com>
2025-08-08 15:08:44 +0100

    refactor: consolidate mini-nvim config

--- private_dot_config/nvim/lua/src/plugins/mini-move.lua
+++
@@ -1,5 +0,0 @@
-require("mini.move").setup({
-	options = {
-		reindent_linewise = false,
-	},
-})
--- private_dot_config/nvim/lua/src/plugins/nvim-treesitter.lua
+++ private_dot_config/nvim/lua/src/plugins/nvim-treesitter.lua
@@ -40,9 +40,7 @@ require("nvim-treesitter.configs").setup({
 	indent = { enable = false },
 })
 
--- Disable injections in 'lua' language. In Neovim<0.9 it is
--- `vim.treesitter.query.set_query()`; in Neovim>=0.9 it is
--- `vim.treesitter.query.set()`.
+-- Disable injections in 'lua' language
 local ts_query = require("vim.treesitter.query")
 local ts_query_set = ts_query.set or ts_query.set_query
 ts_query_set("lua", "injections", "")
