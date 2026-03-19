4f2f282b7cbe3025c79898d47c768bb778a2652c
Kaif Ahmed <github.banister767@passinbox.com>
2026-03-19 22:19:38 +0000

    feat: add audacity and kid3

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,62 +1,64 @@
 { pkgs, user, lib, isPersonal, ... }:
 
 {
-  users.users.${user} = {
-    name = "${user}";
-    home = "/Users/${user}";
-  };
+	users.users.${user} = {
+		name = "${user}";
+		home = "/Users/${user}";
+	};
 
-  system.primaryUser = "${user}";
+	system.primaryUser = "${user}";
 
-  # System-wide packages
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-  ];
+	# System-wide packages
+	environment.systemPackages = with pkgs; [
+		git
+		vim
+	];
 
-  environment.systemPath = [ "/opt/homebrew/bin" ];
+	environment.systemPath = [ "/opt/homebrew/bin" ];
 
-  # Homebrew Packages
-  homebrew = {
-    enable = true;
+	# Homebrew Packages
+	homebrew = {
+		enable = true;
 
-    onActivation.autoUpdate = true;
-    onActivation.upgrade = true;
+		onActivation.autoUpdate = true;
+		onActivation.upgrade = true;
 
-    casks = lib.optionals (isPersonal) [
-      "appcleaner"
-      "netnewswire"
-      "proton-drive"
-      "proton-mail"
-      "proton-pass"
-      "protonvpn"
-      "steam"
-    ];
+		casks = lib.optionals (isPersonal) [
+			"appcleaner"
+			"audacity"
+			"kid3"
+			"netnewswire"
+			"proton-drive"
+			"proton-mail"
+			"proton-pass"
+			"protonvpn"
+			"steam"
+		];
 
-    # Mac App Store Applications
-    masApps = {
-      # Installed on both Work and Personal machines
-      "Ghostery" = 6504861501;
-      "JSON Peep for Safari" = 1458969831;
-    } // lib.optionalAttrs (isPersonal) {
-      # Installed ONLY on Personal machines
-      "Proton Pass for Safari" = 6502835663;
-      "djay - DJ App & AI Mixer" = 450527929;
-    };
-  };
+		# Mac App Store Applications
+		masApps = {
+			# Installed on both Work and Personal machines
+			"Ghostery" = 6504861501;
+			"JSON Peep for Safari" = 1458969831;
+		} // lib.optionalAttrs (isPersonal) {
+			# Installed ONLY on Personal machines
+			"Proton Pass for Safari" = 6502835663;
+			"djay - DJ App & AI Mixer" = 450527929;
+		};
+	};
 
 
-  # Fonts
-  fonts.packages = with pkgs; [
-    nerd-fonts._0xproto
-  ];
+	# Fonts
+	fonts.packages = with pkgs; [
+		nerd-fonts._0xproto
+	];
 
-  # Nix Configuration
-  nix.enable = false;
+	# Nix Configuration
+	nix.enable = false;
 
-  # Enable Zsh (required for MacOS to not complain, even if you use Fish/Bash)
-  programs.zsh.enable = true;
+	# Enable Zsh (required for MacOS to not complain, even if you use Fish/Bash)
+	programs.zsh.enable = true;
 
-  system.stateVersion = 6;
-  nixpkgs.hostPlatform = "aarch64-darwin";
+	system.stateVersion = 6;
+	nixpkgs.hostPlatform = "aarch64-darwin";
 }
