03bd6fd4e57bcfcdcf06439c87cfe54fd292eb57
Kaif Ahmed <github.banister767@passinbox.com>
2026-08-16 21:38:56 +0100

    fix: resolve backup issue

--- flake.nix
+++ flake.nix
@@ -144,6 +144,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = ".bak";
             home-manager.extraSpecialArgs = {
               user = thinkcentre-config.username;
               gitEmail = thinkcentre-config.gitEmail;
@@ -171,6 +172,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = ".bak";
             home-manager.extraSpecialArgs = {
               user = thinkpad-config.username;
               gitEmail = thinkpad-config.gitEmail;
